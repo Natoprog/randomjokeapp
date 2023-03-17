e0e4a530ed9d07012470e02551a8bd32c7a4e548
Natoprog <nata2004no@gmail.com>
2023-03-17 22:00:46 +0100

    Update app and add new features like dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 import {useState, useEffect} from 'react'
 import Head from 'next/head'
-import HomapegeSkeleton from "../components/HomePageSkeleton"
+import HomapegeSkeleton from "../components/HomePageSkeleton";
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from 'next/link';
 
 export default function HomePage() {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
-
+  const [isDark, setTheme] = useState(true)
 
   useEffect(() => {
     getData()
   }, []);
+
+  useEffect(()=> {
+    if(!isDark) {
+      document.documentElement.setAttribute('data-theme', 'light')
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark')
+    }
+  }, [isDark])
 
   const getData = () => {
       setLoading(true)
@@ -21,6 +33,11 @@ export default function HomePage() {
       })
     }
 
+  const handleChangeTheme = () => {
+    setTheme(!isDark)
+    }
+
+  const footerDate = new Date().getFullYear()
 
   return (
     <>
@@ -28,16 +45,29 @@ export default function HomePage() {
         <title>Programing jokes</title>
       </Head>
       <div className='container'>
-      <h1 className='app-topic'>Programing Jokes</h1>
-        <div className='container-inner'>
-            {loading ? <HomapegeSkeleton /> :
-            <>
-            {data.map(joke => <h3 className='question'>{joke.question}</h3>)}
-            {data.map(joke => <h4 className='punchline'>{joke.punchline}</h4>)}
-            </>
-            }
-            <button className='next-btn' onClick={getData}>Next Joke</button>
+        <header>
+        <h1 className='app-topic'>Programing Jokes</h1>
+          <div className='app-theme'>
+            <button className='toggle-btn' onClick={handleChangeTheme}>
+              {!isDark ? <DarkModeIcon fontSize='large'/> : <LightModeIcon fontSize='large'/>}
+            </button>
           </div>
+        </header>
+        <div className='container-inner'>
+              {loading ? <HomapegeSkeleton /> :
+                <>
+                {data.map((joke, i) => <h3 key={i} className='question'>{joke.question}</h3>)}
+                {data.map((joke, i) => <h4 key={i} className='punchline'>{joke.punchline}</h4>)}
+                </>
+              }
+              <button className='next-btn' onClick={getData}>Next Joke</button>
+          </div>
+
+        <footer>
+            <span>&copy; {footerDate}</span>
+            <Link href="https://github.com/Natoprog/randomjokeapp" className='footer-link' target='_blank'><GitHubIcon sx={{color: isDark ? 'white' : 'black' }}/></Link>
+
+        </footer>
       </div>
     </>
   )
