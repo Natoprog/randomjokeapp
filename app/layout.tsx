e693423c2aaa4fc78eeb387ea1f9a861c67a9d0c
Natoprog <natan.lucz@gmail.com>
2025-03-03 19:00:55 +0100

    Update dependencies and add typescript

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "../styles/style.css";
+import "../styles/HomePageSkeleton.css";
+
+// either Static metadata
+export const metadata = {
+  title: "Programing jokes",
+  description: "Random programing jokes",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
